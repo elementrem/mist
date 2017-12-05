0e93053b8342f17be29c7fda50744afa448f6a30
elementrem <elementrem@gmail.com>
2017-12-06 06:53:29 +0900

    Revert "prepare 0.9 update"
    This reverts commit a6eb2d15c343364134c852a2954fbb0b796f9297.

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ var options = minimist(process.argv.slice(2), {
     string: ['platform','walletSource'],
     default: {
         platform: 'all',
-        walletSource: 'devlab'
+        walletSource: 'master'
     }
 });
 
@@ -47,18 +47,18 @@ var filenameLowercase = 'mist';
 var filenameUppercase = 'Mist';
 var applicationName = 'Mist'; 
 var electronVersion = require('electron-prebuilt/package.json').version;
-var geleVersion = '1.5.99';
+var geleVersion = '1.5.97';
 var nodeUrls = {
     'mac-x64': {
-        url: 'https://github.com/elementrem/go-elementrem/releases/download/v1.5.99/mac-gele-1.5.99-14052db.zip',
+        url: 'https://github.com/elementrem/go-elementrem/releases/download/v1.5.97/mac-gele-1.5.97-98bb59.zip',
         ext: 'zip'
     },
     'linux-x64': {
-        url: 'https://github.com/elementrem/go-elementrem/releases/download/v1.5.99/linux-64bit-gele-1.5.99-14052db.zip',
+        url: 'https://github.com/elementrem/go-elementrem/releases/download/v1.5.97/linux-64bit-gele-1.5.97-98bb59c.zip',
         ext: 'zip',
     },
     'win-x64': {
-        url: 'https://github.com/elementrem/go-elementrem/releases/download/v1.5.99/windows-64bit-gele-1.5.99-14052db.zip',
+        url: 'https://github.com/elementrem/go-elementrem/releases/download/v1.5.97/windows-64bit-gele-1.5.97-98bb59c.zip',
         ext: 'zip',
     },
 };
